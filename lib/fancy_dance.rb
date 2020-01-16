b665ea06f32f81d95ce917584743e6a310cc0994
jhuang429 <jhuang9240@gmail.com>
2020-01-16 14:51:46 -0500

    Done.

---
+++ lib/fancy_dance.rb
@@ -0,0 +1,16 @@
+require_relative './class_methods_module.rb'
+require_relative './dance_module.rb'
+
+
+module FancyDance
+    module InstanceMethods
+        include Dance 
+    end
+
+    module ClassMethods
+            def metadata
+              "This class produces objects that love to dance."
+            end
+    end
+
+end
